38fe6c5f854363ca04d9072a444a79a65dd131fc
matheus-or <matheusolivvee@gmail.com>
2022-06-15 11:34:09 -0300

    Adicionando exercícios.

---
+++ S10-A87/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int p;
+		p = 10;
+		System.out.println(p);
+		
+		char p2;
+		p2 = 's';
+		System.out.println(p2);
+		
+		String p3;
+		p3 = "Teste de String !!!";
+		System.out.println(p3);
+	}
+}
