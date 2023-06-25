2ee8f073f0e1fe0fa7f416b897c35701b16951ac
Matheus <matheusolivvee@gmail.com>
2023-06-25 11:48:37 -0300

    Aula sobre listas (List);

---
+++ S10-A97/src/main/java/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+
+        List<Integer> list = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+
+    }
+
+}
