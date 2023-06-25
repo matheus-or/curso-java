c19f8119d1e5cc063d364e838da0758acb9db4eb
Matheus <matheusolivvee@gmail.com>
2023-06-25 10:44:48 -0300

    Desafio "Pensionato"

---
+++ S10-E02/src/main/java/src/application/Pensionato.java
@@ -0,0 +1,51 @@
+package src.application;
+
+import br.com.matheus.model.Hospede;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Pensionato {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Hospede[] vect = new Hospede[10];
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("Rent #" + (i + 1));
+            sc.nextLine();
+
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+
+            System.out.print("Email: ");
+            String email = sc.next();
+
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+
+            vect[room] = new Hospede(name, email);
+
+            System.out.println();
+        }
+
+        System.out.println("Busy rooms:");
+
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] != null) {
+                System.out.println(i +": " + vect[i]);
+            }
+        }
+
+        sc.close();
+    }
+}
---
+++ commons/src/main/java/br/com/matheus/model/Hospede.java
@@ -0,0 +1,42 @@
+package br.com.matheus.model;
+
+public class Hospede {
+
+    private String name;
+    private String email;
+    private int room;
+
+    public Hospede(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRoom() {
+        return room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + email;
+    }
+}
