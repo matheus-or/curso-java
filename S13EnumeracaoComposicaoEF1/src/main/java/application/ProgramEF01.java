337caedfc883a53d20ec568c09c4e61395d74737
Matheus <matheusolivvee@gmail.com>
2023-08-14 09:17:39 -0300

    Primeiro commit dessa branch

--- S13EnumeracaoComposicaoEF1/src/main/java/application/ProgramEF01.java
+++ S13EnumeracaoComposicaoEF1/src/main/java/application/ProgramEF01.java
@@ -1,11 +1,30 @@
 package application;
 
-import entities.OrderItem;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ProgramEF01 {
 
 	public static void main(String[] args) {
-//		OrderItem item = new OrderItem();
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter cliente data:");
+		
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		String dataNascimento = sc.nextLine();
+
+		System.out.println("Enter order data:");
+		
+
+		sc.close();
 	}
 
 }
