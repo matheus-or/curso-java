a57abf079a344aeee934486a96e49e28d8987297
Matheus <matheusolivvee@gmail.com>
2024-10-03 16:42:52 -0300

    Primeiro exemplo seção de trabalhando com arquivos.

--- S15-TrabalhandoComArquivos/src/main/java/application/ProgramExe01.java
+++ S15-TrabalhandoComArquivos/src/main/java/application/ProgramExe01.java
@@ -1,27 +1,27 @@
 package application;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
-public class Program2 {
+public class ProgramExe01 {
 
 	public static void main(String[] args) {
 
-		File file = new File("C:\\testes\\in.txt");
+		File file = new File("c://testes/in.txt");
 		Scanner sc = null;
 		try {
 			sc = new Scanner(file);
 			while (sc.hasNextLine()) {
 				System.out.println(sc.nextLine());
 			}
-		} catch (FileNotFoundException e) {
-			System.out.println("Error opening file: " + e.getMessage());
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		} finally {
-			if(sc != null) {
+			if (sc != null) {
 				sc.close();
 			}
-			System.out.println("Finally block executed");
 		}
 	}
+
 }
